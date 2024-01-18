596c0ba8c47f5b25262fdc290e14ee6d5b8c9819
Rodmansito <78875773+rodman751@users.noreply.github.com>
2024-01-17 20:27:20 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@
 
 const express = require('express')
 const app = express()
+const cors = require('cors');
 
 //middlewears
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 //routes
 app.use(require('../routes/Rindex'));
